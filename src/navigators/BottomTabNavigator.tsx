30ed541a69576f9a728416a82f9021560b81da23
Ahyoung-Kim <eileel@naver.com>
2024-01-12 17:46:00 +0900

    Design: 바텀 탭 디자인 변경

--- src/navigators/BottomTabNavigator.tsx
+++ src/navigators/BottomTabNavigator.tsx
@@ -19,13 +19,15 @@ const BottomTabNavigator = () => {
         tabBarStyle: {
           backgroundColor: color.COLOR_WHITE_BACKGROUND,
           height: 80,
-          shadowColor: color.COLOR_BOX_SHADOW,
-          shadowOffset: {
-            width: 0,
-            height: -3,
-          },
-          shadowOpacity: 0.2,
-          elevation: 3,
+          borderTopWidth: 2,
+          borderTopColor: color.COLOR_MAIN,
+          //   shadowColor: color.COLOR_BOX_SHADOW,
+          //   shadowOffset: {
+          //     width: 0,
+          //     height: -3,
+          //   },
+          //   shadowOpacity: 0.2,
+          //   elevation: 3,
         },
         tabBarItemStyle: {
           marginBottom: 5,
