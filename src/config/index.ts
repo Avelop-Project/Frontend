1bd1132581a77dac694a91717545f7ac29a5cf9f
Ahyoung-Kim <eileel@naver.com>
2024-02-06 17:09:38 +0900

    Test: API 테스트

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,5 @@
 export const WEBVIEW_URI = "https://avelop-webview.vercel.app/";
 export const LOCAL_WEBVIEW_URI = "http://172.29.105.203:3000";
+
+export const API_URL = "http://localhost:8080/api";
+export const TEST_API_URL = "http://localhost:8080/mock_api";
--- src/screens/cardsList/CardsListScreen.tsx
+++ src/screens/cardsList/CardsListScreen.tsx
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, FlatList, View, Pressable, Text } from "react-native";
+
+import axios from "axios";
+import { TEST_API_URL } from "../../config";
 
 import MainLayout from "../../layout/MainLayout";
 import PriceFilter from "../../components/cardsList/PriceFilter";
 import CardsSet from "../../components/cardsList/CardsSet";
 
 const CardsListScreen = () => {
+  const testAPI = async () => {
+    try {
+      const res = await axios.get(`${TEST_API_URL}/card/browse`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    testAPI();
+  }, []);
+
   return (
     <>
       <MainLayout
